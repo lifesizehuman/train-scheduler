ef62dd54382e8e0222b6d7fa8e3c8beef5270873
Slater Combes <slatercombes@Slaters-MBP.attlocal.net>
2017-08-08 16:52:56 -0700

    set up table and initialize firebase

---
+++ js/logic.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+var config = {
+    apiKey: "AIzaSyCCFTtP84qjShv7EzKpmVSSLarJoDa_mc4",
+    authDomain: "train-scheduler-5cc49.firebaseapp.com",
+    databaseURL: "https://train-scheduler-5cc49.firebaseio.com",
+    projectId: "train-scheduler-5cc49",
+    storageBucket: "train-scheduler-5cc49.appspot.com",
+    messagingSenderId: "379506602196"
+  };
+  firebase.initializeApp(config);
+
+  
+
+}
