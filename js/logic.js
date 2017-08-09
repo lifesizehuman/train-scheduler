37b6032040661415b0d2c15aa43eab4cf7e19eb4
Slater Combes <slatercombes@Slaters-MBP.attlocal.net>
2017-08-08 17:08:07 -0700

    set up logic and correct table

--- js/logic.js
+++ js/logic.js
@@ -1,15 +1,52 @@
 $(document).ready(function() {
 
-var config = {
-    apiKey: "AIzaSyCCFTtP84qjShv7EzKpmVSSLarJoDa_mc4",
-    authDomain: "train-scheduler-5cc49.firebaseapp.com",
-    databaseURL: "https://train-scheduler-5cc49.firebaseio.com",
-    projectId: "train-scheduler-5cc49",
-    storageBucket: "train-scheduler-5cc49.appspot.com",
-    messagingSenderId: "379506602196"
-  };
-  firebase.initializeApp(config);
+            var config = {
+                apiKey: "AIzaSyCCFTtP84qjShv7EzKpmVSSLarJoDa_mc4",
+                authDomain: "train-scheduler-5cc49.firebaseapp.com",
+                databaseURL: "https://train-scheduler-5cc49.firebaseio.com",
+                projectId: "train-scheduler-5cc49",
+                storageBucket: "train-scheduler-5cc49.appspot.com",
+                messagingSenderId: "379506602196"
+            };
+            firebase.initializeApp(config);
 
-  
+            database = firebase.database();
 
-}
+            $('#add-train').on('click', function(event) {
+                event.preventDefault();
+
+                var name = $('#train-input').val().trim();
+                var destination = $('#destination-input').val().trim();
+                var time = $('#time-input').val().trim();
+                var frequency = $('#frequency-input').val().trim();
+
+                database.ref().push({
+                    name: name,
+                    destination: destination,
+                    time: time,
+                    frequency: frequency
+
+                })
+
+            })
+
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+
+	var trainName = childSnapshot.val().name;
+  var trainDestination = childSnapshot.val().destination;
+  var trainTime = childSnapshot.val().time;
+  var trainFrequency = childSnapshot.val().frequency;
+
+  $("#train-table > tbody").append(
+  	"<tr><td>" + trainName + 
+  	"</td><td>" + trainDestination + 
+  	"</td><td>" + trainTime + 
+  	"</td><td>" + trainFrequency + 
+  	// "</td><td>" + nextArrival + 
+  	// "</td><td>" + minutesAway + 
+  	"</td></tr>");
+
+        })
+
+})
