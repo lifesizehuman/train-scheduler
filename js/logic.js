d4075885c356fb4afa62edfece27189bca871066
Slater Combes <slatercombes@Slaters-MBP.attlocal.net>
2017-08-08 17:17:32 -0700

    set up train scheduler logic and plug into table

--- js/logic.js
+++ js/logic.js
@@ -1,52 +1,63 @@
 $(document).ready(function() {
 
-            var config = {
-                apiKey: "AIzaSyCCFTtP84qjShv7EzKpmVSSLarJoDa_mc4",
-                authDomain: "train-scheduler-5cc49.firebaseapp.com",
-                databaseURL: "https://train-scheduler-5cc49.firebaseio.com",
-                projectId: "train-scheduler-5cc49",
-                storageBucket: "train-scheduler-5cc49.appspot.com",
-                messagingSenderId: "379506602196"
-            };
-            firebase.initializeApp(config);
+    var config = {
+        apiKey: "AIzaSyCCFTtP84qjShv7EzKpmVSSLarJoDa_mc4",
+        authDomain: "train-scheduler-5cc49.firebaseapp.com",
+        databaseURL: "https://train-scheduler-5cc49.firebaseio.com",
+        projectId: "train-scheduler-5cc49",
+        storageBucket: "train-scheduler-5cc49.appspot.com",
+        messagingSenderId: "379506602196"
+    };
+    firebase.initializeApp(config);
 
-            database = firebase.database();
+    database = firebase.database();
 
-            $('#add-train').on('click', function(event) {
-                event.preventDefault();
+    $('#add-train').on('click', function(event) {
+        event.preventDefault();
 
-                var name = $('#train-input').val().trim();
-                var destination = $('#destination-input').val().trim();
-                var time = $('#time-input').val().trim();
-                var frequency = $('#frequency-input').val().trim();
+        var name = $('#train-input').val().trim();
+        var destination = $('#destination-input').val().trim();
+        var time = $('#time-input').val().trim();
+        var frequency = $('#frequency-input').val().trim();
 
-                database.ref().push({
-                    name: name,
-                    destination: destination,
-                    time: time,
-                    frequency: frequency
-
-                })
-
-            })
-
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-
-
-	var trainName = childSnapshot.val().name;
-  var trainDestination = childSnapshot.val().destination;
-  var trainTime = childSnapshot.val().time;
-  var trainFrequency = childSnapshot.val().frequency;
-
-  $("#train-table > tbody").append(
-  	"<tr><td>" + trainName + 
-  	"</td><td>" + trainDestination + 
-  	"</td><td>" + trainTime + 
-  	"</td><td>" + trainFrequency + 
-  	// "</td><td>" + nextArrival + 
-  	// "</td><td>" + minutesAway + 
-  	"</td></tr>");
+        database.ref().push({
+            name: name,
+            destination: destination,
+            time: time,
+            frequency: frequency
 
         })
+
+    })
+
+    database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+
+        var trainName = childSnapshot.val().name;
+        var trainDestination = childSnapshot.val().destination;
+        var trainTime = childSnapshot.val().time;
+        var trainFrequency = childSnapshot.val().frequency;
+
+        var currentTime = moment();
+
+        var convertedTime = moment(currentTime, "hh:mm").subtract(1, "years");
+
+       var differenceTime = moment().diff(moment(convertedTime), "minutes");
+
+       var remainderTime = differenceTime % trainFrequency;
+
+       var minutesAway = trainFrequency - remainderTime;
+
+       var nextArrival = moment().add(minutesAway, "minutes");
+
+        $("#train-table > tbody").append(
+            "<tr><td>" + trainName +
+            "</td><td>" + trainDestination +
+            "</td><td>" + trainFrequency +
+            "</td><td>" + nextArrival.format("hh:mm") +
+            "</td><td>" + minutesAway +
+            "</td></tr>");
+
+    })
 
 })
