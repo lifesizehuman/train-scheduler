cc8aceec43e4e0445a590f28cedbd761eea8116c
Slater Combes <slatercombes@Slaters-MBP.attlocal.net>
2017-08-08 17:35:34 -0700

    adjust for current time

--- js/logic.js
+++ js/logic.js
@@ -46,13 +46,13 @@ $(document).ready(function() {
         var differenceTime = moment().diff(moment(convertedTime), "minutes");
         var remainderTime = differenceTime % trainFrequency;
         var minutesAway = trainFrequency - remainderTime;
-        var nextArrival = moment(currentTime).add(minutesAway, "minutes");
+        var nextArrival = moment().add(minutesAway, "minutes");
 
         $("#train-table > tbody").append(
             "<tr><td>" + trainName +
             "</td><td>" + trainDestination +
             "</td><td>" + trainFrequency +
-            "</td><td>" + nextArrival.format("hh:mm") +
+            "</td><td>" + moment(nextArrival).format("hh:mm") +
             "</td><td>" + minutesAway +
             "</td></tr>");
     })
