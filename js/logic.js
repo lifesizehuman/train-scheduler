ac6cd87799c6624fbfe69fa116674f7a3c8aa315
Slater Combes <combes.slater@gmail.com>
2018-07-31 11:40:35 -0700

    Update logic.js

--- js/logic.js
+++ js/logic.js
@@ -1,10 +1,10 @@
 var config = {
-    apiKey: "AIzaSyCCFTtP84qjShv7EzKpmVSSLarJoDa_mc4",
-    authDomain: "train-scheduler-5cc49.firebaseapp.com",
-    databaseURL: "https://train-scheduler-5cc49.firebaseio.com",
-    projectId: "train-scheduler-5cc49",
-    storageBucket: "train-scheduler-5cc49.appspot.com",
-    messagingSenderId: "379506602196"
+    apiKey: "********",
+    authDomain: "********",
+    databaseURL: "********",
+    projectId: "********",
+    storageBucket: "********",
+    messagingSenderId: "********"
 };
 firebase.initializeApp(config);
 
